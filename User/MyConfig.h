198b251cb5225e46df1e956df1d4fa390ca7c625
MoonlightShadow1 <luboemail@126.com>
2018-08-28 08:38:16 +0800

    由于原理图更新 导致IO引脚需要重新配置； IO引脚重新配置完成，代码通过，测试通过

--- User/MyConfig.h
+++ User/MyConfig.h
@@ -24,38 +24,38 @@
 
 
 /*输入IO引脚定义*/
-sbit IO1 = P1^4;
-sbit IO2 = P1^2;
-sbit IO3 = P0^3;
-sbit IO4 = P0^1;
-sbit IO5 = P5^4;
-sbit IO6 = P2^3;
-sbit IO7 = P2^1;
-sbit IO8 = P2^5;
+sbit IO1 = P5^4;//IO1 = P1^4;
+sbit IO2 = P1^6;//IO2 = P1^2;
+sbit IO3 = P1^3;//IO3 = P0^3;
+sbit IO4 = P1^1;//IO4 = P0^1;
+sbit IO5 = P3^2;//IO5 = P5^4;
+sbit IO6 = P3^4;//IO6 = P2^3;
+sbit IO7 = P2^4;//IO7 = P2^1;
+sbit IO8 = P0^3;//IO8 = P2^5;
 
 /*IO使能拨码开关引脚定义*/
-sbit IO1_En = P2^4;
-sbit IO2_En = P3^1;
-sbit IO3_En = P3^0;
-sbit IO4_En = P3^5;
-sbit IO5_En = P3^4;
-sbit IO6_En = P2^0;
-sbit IO7_En = P2^2;
-sbit IO8_En = P5^5;
+sbit IO1_En = P0^2;//IO1_En = P2^4;
+sbit IO2_En = P0^0;//IO2_En = P3^1;
+sbit IO3_En = P2^7;//IO3_En = P3^0;
+sbit IO4_En = P0^1;//IO4_En = P3^5;
+sbit IO5_En = P2^6;//IO5_En = P3^4;
+sbit IO6_En = P2^5;//IO6_En = P2^0;
+sbit IO7_En = P3^5;//IO7_En = P2^2;
+sbit IO8_En = P3^3;//IO8_En = P5^5;
 
 /*RS485地址拨码开关引脚定义*/
-sbit Addr1 = P1^5;
-sbit Addr2 = P1^3;
-sbit Addr3 = P1^1;
-sbit Addr4 = P1^0;
-sbit Addr5 = P0^2;
-sbit Addr6 = P0^0;
+sbit Addr1 = P5^5;//Addr1 = P1^5;
+sbit Addr2 = P1^7;//Addr2 = P1^3;
+sbit Addr3 = P1^5;//Addr3 = P1^1;
+sbit Addr4 = P1^4;//Addr4 = P1^0;
+sbit Addr5 = P1^2;//Addr5 = P0^2;
+sbit Addr6 = P1^0;//Addr6 = P0^0;
 
 
 
 /*继电器引脚定义*/
-sbit Relay1 = P3^3;
-sbit Relay2 = P3^2;
+sbit Relay1 = P2^1;//Relay1 = P3^3;
+sbit Relay2 = P2^0;//Relay2 = P3^2;
 
 
 
